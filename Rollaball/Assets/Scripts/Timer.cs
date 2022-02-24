7787a349a8d942e36e0b455c769bf54ba83679e8
DavBatts <dsbatts13@gmail.com>
2022-02-24 03:05:16 -0500

    adds timer

--- Rollaball/Assets/Scripts/Timer.cs
+++ Rollaball/Assets/Scripts/Timer.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float currentTime;
+    public float startingTime = 20f;
+    public GameObject LoseTextObject;
+
+    [SerializeField] Text countdownText;
     void Start()
     {
-        
+        currentTime = startingTime;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            LoseTextObject.SetActive(true);
+        }
     }
 }
